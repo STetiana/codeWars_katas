64e675ae4747de2635a3c6cb9844393143cb3783
STetiana <tetyanasahaidak@gmail.com>
2022-09-18 19:50:29 +0200

    the solved task

--- reversing_a_process/main.py
+++ reversing_a_process/main.py
@@ -1,1 +1,22 @@
-return 0
+def decode(r):
+    num = ""
+    text = ""
+    decoded_text = ""
+    for l in r:
+        if l.isdigit():
+            num+=l
+        else:
+            text += l
+    num = int(num)
+
+    for c in text:
+        err = False
+        for i in range(26):
+            if num * i % 26 == ord(c) - ord('a') and err == False:
+                decoded_text += chr(i+97)
+                err = True
+            elif err == True and num * i % 26 == ord(c) - ord('a'):
+                return "Impossible to decode"
+            else:
+                continue
+    return decoded_text
